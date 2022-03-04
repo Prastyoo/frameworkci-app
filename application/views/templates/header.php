b733a9aa1eddf0e17f615e4ea4290ba523833847
Prasetyo <panduprasetyoo2404@gmail.com>
2022-03-04 17:03:08 +0700

    initial commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title> Restoran Mie Ayam & Bakso Pandu </title>
+  </head>
+  <body>
+
+  <!-- As a link -->
+    <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#"> Restoran Mie Ayam & Bakso Pandu </a>
+    </div>
+    </nav>
+
+    <!-- As a heading -->
+    <nav class="navbar navbar-light bg-light">
+    <div class="container">
+        <div class="navbar-brand mb-0 h1 mx-auto"> Selamat Datang </div>
+    </div>
+    <div class="container">
+        <div class="navbar-brand mb-0 h1 mx-auto"> Selamat Menikmati Hidangan yang telah disajikan</div>
+    </div>
+    </nav>
